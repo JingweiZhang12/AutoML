69162676ae1ee034aec911e52e9c99aa0e77bf96
jingweizhang <jingweizhang2017@gmail.com>
2019-08-28 22:15:17 +0800

    new

--- mypath.py
+++ mypath.py
@@ -2,15 +2,15 @@ class Path(object):
     @staticmethod
     def db_root_dir(dataset):
         if dataset == 'pascal':
-            return '/path/to/datasets/VOCdevkit/VOC2012/'  # folder that contains VOCdevkit/.
+            return '/home/zhangjw/Datasets/VOCdevkit/VOC2012/'  # folder that contains VOCdevkit/.
         elif dataset == 'sbd':
-            return '/path/to/datasets/benchmark_RELEASE/'  # folder that contains dataset/.
+            return '/homw/zhangjw/Datasets/benchmark_RELEASE/'  # folder that contains dataset/.
         elif dataset == 'cityscapes':
-            return '/data/deeplearning/cityscapes/'     # foler that contains leftImg8bit/
+            return '/home/zhangjw/Datasets/cityscapes/'     # foler that contains leftImg8bit/
         elif dataset == 'kd':
-            return '/data/deeplearning/cityscapes/'
+            return '/home/zhangjw/Datasets/cityscapes/'
         elif dataset == 'coco':
-            return '/data/deeplearning/dataset/coco2017'
+            return '/home/zhangjw/Datasets/coco2017'
         else:
             print('Dataset {} not available.'.format(dataset))
             raise NotImplementedError
